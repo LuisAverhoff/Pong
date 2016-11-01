35b0cc1d59960d41f40963df62dcff002e05cc3f
Luis Manuel Averhoff <laver008@fiu.edu>
2016-11-01 15:30:15 -0400

    Update Paddle.cpp

--- Pong/Src/Paddle.cpp
+++ Pong/Src/Paddle.cpp
@@ -1,4 +1,13 @@
 #include "Paddle.h"
+#include "Ball.h"
+#include <cmath>
+#include <random>
+
+namespace PaddleRandomNumGenerator
+{
+	std::random_device randDevice;
+	std::default_random_engine randomGenerator(randDevice());
+}
 
 Paddle::Paddle(float xPos, float yPos, const int WIDTH, const int HEIGHT) : 
 	PADDLE_X_POSITION(xPos), paddleYPosition(yPos),
@@ -12,18 +21,51 @@ float Paddle::getYPosition() const
 	return  paddleYPosition; 
 }
 
-void Paddle::moveUp(float timeStep)
+void Paddle::moveUp(float newYPosition)
 {
 	if (paddleYPosition > 10) // Height of the top rectangle.
 	{
-		paddleYPosition -= (Y_VELOCITY * timeStep);
+		paddleYPosition -= newYPosition;
 	}
 }
 
-void Paddle::moveDown(float timeStep, const int SCREEN_HEIGHT)
+void Paddle::moveDown(float newYPosition, const int SCREEN_HEIGHT)
 {
 	if ((paddleYPosition + PADDLE_HEIGHT) < SCREEN_HEIGHT)
 	{
-		paddleYPosition += (Y_VELOCITY * timeStep);
+		paddleYPosition += newYPosition;
+	}
+}
+
+void Paddle::AIMove(Ball *ball, float timestep, const int SCREEN_HEIGHT)
+{
+	float impactDistance, impactTime, targetY;
+    
+	float ballX = ball->getCenterX();
+	float ballY = ball->getCenterY();
+	float ballDx = ball->getVelocityX();
+	float ballDy = ball->getVelocityY();
+
+	std::uniform_real_distribution<float> speedFactor(0.45f, 0.85f); // A speed factor between 0.65 and 0.85.
+	float AISpeedUpFactor = speedFactor(PaddleRandomNumGenerator::randomGenerator);
+
+	impactDistance = PADDLE_X_POSITION - ballX;
+	impactTime = impactDistance / (ballDx * AISpeedUpFactor * 1000.0f);
+	targetY = ballY + (ballDy * AISpeedUpFactor * 1000.0f) * impactTime;
+
+	if (abs(targetY - (paddleYPosition + (PADDLE_HEIGHT / 2.0f))) < 10.0f)
+	{
+		return;
+	}
+
+	if (targetY > paddleYPosition + (PADDLE_HEIGHT / 2))
+	{
+		float newYPosition = Y_VELOCITY * AISpeedUpFactor * timestep;
+		moveDown(newYPosition, SCREEN_HEIGHT);
+	}
+	else if (targetY < paddleYPosition + (PADDLE_HEIGHT / 2))
+	{
+		float newYPosition = Y_VELOCITY * AISpeedUpFactor * timestep;
+		moveUp(newYPosition);
 	}
 }
